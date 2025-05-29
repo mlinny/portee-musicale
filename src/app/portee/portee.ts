daa29956be3ed8a28fbcfa8698d305b50727a1d4
marc lehmann <lehmannmarc@outlook.fr>
2025-05-29 17:23:21 +0200

    Correction sur la position des notes et la taille de la portée

--- src/app/portee/portee.ts
+++ src/app/portee/portee.ts
@@ -11,13 +11,13 @@ import { FormsModule } from '@angular/forms';
 })
 export class PorteeComponent {
   public notes = [
-    { nom: 'Do', position: 100 },
-    { nom: 'Ré', position: 90 },
-    { nom: 'Mi', position: 80 },
-    { nom: 'Fa', position: 70 },
-    { nom: 'Sol', position: 60 },
-    { nom: 'La', position: 50 },
-    { nom: 'Si', position: 40 }
+    { nom: 'Do', position: 40 },
+    { nom: 'Ré', position: 30 },
+    { nom: 'Mi', position: 20 },
+    { nom: 'Fa', position: 10 },
+    { nom: 'Sol', position: 0 },
+    { nom: 'La', position: -10 },
+    { nom: 'Si', position: -20 }
   ];
 
   noteSelectionnee?: { nom: string, position: number } | null = null;
